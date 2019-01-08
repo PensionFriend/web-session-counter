0911d2181017c89d197c9ddec8bfcc0babe2201f
pavel-jurasek-hypofriend-de <pavel@hypofriend.de>
2019-01-08 11:46:29 +0100

    fix

--- src/WebSessionCounter.js
+++ src/WebSessionCounter.js
@@ -22,18 +22,18 @@ class WebSessionCounter {
 
     get count() {
         if (canUseLocalStorage) {
-            return Number(window.localStorage.getItem('user_web_session_count'));
+            return Number(window.localStorage.getItem('hypofriend_user_web_session_count'));
         }else{
             return NaN;
         }
     }
 
     set count(val) {
-        window.localStorage.setItem('user_web_session_count', val);
+        window.localStorage.setItem('hypofriend_user_web_session_count', val);
     }
 
     get lastActive() {
-        const time = window.localStorage.getItem('user_web_session_last_active');
+        const time = window.localStorage.getItem('hypofriend_user_web_session_last_active');
 
         if (time) {
             return new Date(time);
@@ -43,15 +43,15 @@ class WebSessionCounter {
     }
 
     set lastActive(time) {
-        window.localStorage.setItem('user_web_session_last_active', time.toISOString());
+        window.localStorage.setItem('hypofriend_user_web_session_last_active', time.toISOString());
     }
 
     get lastUtmCampaign() {
-        return window.localStorage.getItem('user_web_session_utm_campaign');
+        return window.localStorage.getItem('hypofriend_user_web_session_utm_campaign');
     }
 
     set lastUtmCampaign(val) {
-        window.localStorage.setItem('user_web_session_utm_campaign', val);
+        window.localStorage.setItem('hypofriend_user_web_session_utm_campaign', val);
     }
 
     get currentUtmCampaign() {
@@ -59,6 +59,36 @@ class WebSessionCounter {
               { utm_campaign = '' } = querystring.parse(query);
 
         return utm_campaign;
+    }
+
+    get lastUtmSource() {
+        return window.localStorage.getItem('hypofriend_user_web_session_utm_source');
+    }
+
+    set lastUtmSource(val) {
+        window.localStorage.setItem('hypofriend_user_web_session_utm_source', val);
+    }
+
+    get currentUtmSource() {
+        const [ path, query = '' ] = window.location.href.split('?'),
+              { utm_source = '' } = querystring.parse(query);
+
+        return utm_source;
+    }
+
+    get lastUtmMedium() {
+        return window.localStorage.getItem('hypofriend_user_web_session_utm_medium');
+    }
+
+    set lastUtmMedium(val) {
+        window.localStorage.setItem('hypofriend_user_web_session_utm_medium', val);
+    }
+
+    get currentUtmMedium() {
+        const [ path, query = '' ] = window.location.href.split('?'),
+              { utm_medium = '' } = querystring.parse(query);
+
+        return utm_medium;
     }
 
     update() {
@@ -70,6 +100,8 @@ class WebSessionCounter {
                 this.count = count + 1;
                 this.lastActive = new Date();
                 this.lastUtmCampaign = this.currentUtmCampaign;
+                this.lastUtmMedium = this.currentUtmMedium;
+                this.lastUtmSource = this.currentUtmSource;
             }
         }
     }
@@ -83,7 +115,9 @@ class WebSessionCounter {
         return [
             (now - time)/1000/60 > 30,
             now.toDateString() !== time.toDateString(),
-            this.lastUtmCampaign !== this.currentUtmCampaign
+            this.lastUtmCampaign !== this.currentUtmCampaign,
+            this.lastUtmSource !== this.currentUtmMedium,
+            this.lastUtmMedium !== this.currentUtmSource
         ].some(b => b);
     }
 }
